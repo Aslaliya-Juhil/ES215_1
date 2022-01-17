f96be8fe9d68cd65c2b16758d2f3b0a215ea77e0
Aslaliya-Juhil <76400010+Aslaliya-Juhil@users.noreply.github.com>
2022-01-17 08:21:07 +0530

    Update ES215_1_1a.cpp

--- ES215_1_1a.cpp
+++ ES215_1_1a.cpp
@@ -12,6 +12,11 @@ unsigned long long fibonacci(int n) {
 }
 int main() {
     auto start = high_resolution_clock::now();
+    // Here, the code was practically at stop after 50th number, so
+    // to avoid waiting for a long time, I am stopping it on the 45th
+    // step because it is already taking ~17 seconds, while printing
+    // 50 numbers was taking ~187 seconds, so using this data, I could
+    // infer that it would take ~1.8 lakh years, which is not practical.
     for(int i = 1; i <= 45; i++) {
         cout << fibonacci(i) << endl;
     }
