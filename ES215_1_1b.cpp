bf7b6e56758e1757c533859bb250c34e5f6cff98
Aslaliya-Juhil <76400010+Aslaliya-Juhil@users.noreply.github.com>
2022-01-22 10:44:11 +0530

    Update ES215_1_1b.cpp

--- ES215_1_1b.cpp
+++ ES215_1_1b.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
-#include<chrono>
 using namespace std;
-using namespace std::chrono;
 
 int main() {
-    unsigned long long x = 0;
-    unsigned long long y = 1;
-    auto start = high_resolution_clock::now();
+    int x = 0;
+    int y = 1;
     for(int i = 2; i < 100; i++) {
-        unsigned long long temp = x + y;
+        int temp = x + y;
         cout << temp << endl;
         x = y;
         y = temp;
     }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<nanoseconds>(stop - start);
-    cout << "time: " << duration.count() << endl;
     return 0;
 }
